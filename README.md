fb893934fed781826966a121ec1ef15846483495
Benjamin Ruston <benjy.ruston@gmail.com>
2020-06-30 13:28:12 +0100

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+Outputs Common Name from SSL certificates when given a list of ip addresses, hostnames or urls.
+```
+Usage of cndump:
+  -c int
+    	number of concurrent requests to make (default 10)
+  -f string
+    	path to list of urls or ip addresses, defaults to stdin if left blank
+  -r	follow redirects (default false)
+  -t int
+    	timeout in seconds (default 5)
+```
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	input := flag.String("f", "", "path to list of urls or ip addresses, defaults to stdin if left blank")
+	concurrent := flag.Int("c", 10, "number of concurrent requests to make")
+	timeout := flag.Int("t", 5, "timeout in seconds")
+	redirect := flag.Bool("r", false, "follow redirects")
+	flag.Parse()
+
+	var f io.ReadCloser
+	if *input == "" {
+		f = os.Stdin
+	} else {
+		file, err := os.Open(*input)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "unable to open input file:", err)
+			os.Exit(1)
+		}
+		f = file
+	}
+	defer f.Close()
+
+	work := make(chan string)
+	go func() {
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			work <- s.Text()
+		}
+		if s.Err() != nil {
+			fmt.Fprintln(os.Stderr, "error while scanning input:", s.Err())
+		}
+		close(work)
+	}()
+
+	client := &http.Client{
+		Timeout: time.Second * time.Duration(*timeout),
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	if !*redirect {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+
+	}
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < *concurrent; i++ {
+		wg.Add(1)
+		go getCommon(client, work, wg)
+	}
+	wg.Wait()
+}
+
+func getCommon(client *http.Client, work chan string, wg *sync.WaitGroup) {
+	for url := range work {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			continue
+		}
+		req.Close = true
+		resp, err := client.Do(req)
+		if err != nil {
+			continue
+		}
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+		if resp.TLS == nil || len(resp.TLS.PeerCertificates) == 0 {
+			continue
+		}
+		fmt.Println(resp.TLS.PeerCertificates[0].Subject.CommonName)
+	}
+	wg.Done()
+}
